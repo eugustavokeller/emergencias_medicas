84bcd775adbbd845fc73a2d72b3ac73683f2545e
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2022-11-04 09:54:28 -0300

    Create computed method mapState

--- src/store.js
+++ src/store.js
@@ -1,3 +1,15 @@
 import Vuex from 'vuex'
 
-export default new Vuex.Store({})
+export default new Vuex.Store({
+    state: {
+        titulo: 'Emergências Médicas',
+        equipe: {
+            enfermeiro: 'Nome do enfermeiro',
+            socorrista: 'Nome do socorrista',
+            medico: 'Nome do medico',
+            carro: 'AAA-1111',
+            telefone: '11 00000-0000',
+            kit_reanimacao: 'Kit 001',
+        }
+    }
+})
